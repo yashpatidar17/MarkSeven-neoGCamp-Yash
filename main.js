d731ed3c790beae3f405c49213d5617e327b6434
Yash Patidar <yashpatidar964@gmail.com>
2023-01-07 12:48:12 +0530

    Added - Logics in Js file

---
+++ main.js
@@ -0,0 +1,17 @@
+var TextArea = document.querySelector("#text-area");
+var Button = document.querySelector("#BtnTranslate");
+var OutPut = document.querySelector("#outputArea");
+
+var translationURL = "https://api.funtranslations.com/translate/valyrian.json";
+
+function getUrl(text){
+    return translationURL + "?text=" + text;
+}
+
+function translationAfterClick(){
+    fetch(getUrl(TextArea.value))
+        .then(response => response.json())
+        .then(json => OutPut.innerText = json.contents.translated)
+}
+
+Button.addEventListener("click",translationAfterClick);
