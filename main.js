d7cdcefbf686640a2e19a5137c5c68afcfa313b8
Yash Patidar <yashpatidar964@gmail.com>
2023-01-15 18:55:22 +0530

    Fix - Added error handler

--- main.js
+++ main.js
@@ -8,10 +8,15 @@ function getUrl(text){
     return translationURL + "?text=" + text;
 }
 
+function errorHandler(error){
+    alert("An error occured please try after some time");
+}
+
 function translationAfterClick(){
     fetch(getUrl(TextArea.value))
         .then(response => response.json())
         .then(json => OutPut.innerText = json.contents.translated)
+        .catch(errorHandler)
 }
 
 Button.addEventListener("click",translationAfterClick);
